35f814b742ba94785c518059a9cfc5375a603a93
Pedro Felipe Gomez <pedro.gomez.2019@upb.edu.co>
2020-08-11 12:34:48 -0500

    Creating Node

--- src/edu/upb/ds/doublelinkedlist/ListNode.java
+++ src/edu/upb/ds/doublelinkedlist/ListNode.java
@@ -1,4 +1,52 @@
 package edu.upb.ds.doublelinkedlist;
 
 public class ListNode {
+    public Object object;
+    public ListNode next;
+    public ListNode before;
+
+    public ListNode(){}
+
+    public ListNode(Object object, ListNode next, ListNode before) {
+        this.object = object;
+        this.next = next;
+        this.before = before;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+
+    public ListNode getBefore() {
+        return before;
+    }
+
+    public void setBefore(ListNode before) {
+        this.before = before;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(object);
+        /*
+        return "ListNode{" +
+                "object=" + object +
+                ", next=" + next +
+                ", before=" + before +
+                '}';
+
+         */
+    }
 }
